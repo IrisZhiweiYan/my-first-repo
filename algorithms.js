97c999e26f500742b99e72143dd6ce1e0fd75191
Iris <irisgogo.yan@gmail.com>
2020-10-24 21:23:18 -0500

    Update algorithms.js

--- algorithms.js
+++ algorithms.js
@@ -3,7 +3,9 @@ onload = function () {
   init();
 };
 
-function doAlgoStep1() {    
+function doAlgoStep1() {
+  run_logging();
+    
   remove("hand");
   remove("go-button");
 
@@ -15,10 +17,7 @@ function doAlgoStep1() {
   arrow.className = "arrow";
   arrow.src = "https://raw.githack.com/hourofci/lessons-dev/master/beginner-lessons/computational-thinking/supplementary/arrow-up.png";
   document.getElementById("highlighted-tree").appendChild(arrow);
-  animate("move-arrow-up 2s", arrow, doAlgoStep2);
-  
-  // logging
-  run_logging();
+  animate("move-arrow-up 2s", arrow, doAlgoStep2); 
 }
 
 function doAlgoStep2() {
